a9a93d1363e58c573abacdc03c27ead587109891
arha06001 <arha06001@E-PW0234JA>
2023-09-15 12:39:31 +0200

    Add project files.

---
+++ Uppgift 2.1 inlämningsuppgift/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Atte
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string favoritbok = Console.ReadLine();
+
+            Console.WriteLine("Din favoritbok är " + favoritbok + " Det är en bra bok");
+        }
+    }
+}
